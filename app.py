1f015c988556d2d0473f2215b1425833d4e7fe90
Cauã Giovani <caua.g.andrade7@aluno.senai.br>
2026-02-12 17:22:44 -0300

    commit 2

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template,request,redirect,session
+import mysql
+import mysql.connector
 
 app = Flask(__name__)
 
 @app.route("/")
-
+@app.route("/home",methods=["GET"])
 def pag_principal():
-    return render_template("principal.html")
+    conexao=mysql.connector.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        password="root",
+        database="Musicablo"
+    )
+
+    cursor = conexao.cursor(dictionary=True)
+    cursor.execute("SELECT id_musica,cantor,duracao,nome,url_capa,nome_genero FROM musica;")
+    musicas=cursor.fetchall()
+
+    return render_template("principal.html",musicas=musicas)
 
 @app.route("/admin")
 
