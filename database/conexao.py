197db5a60e613e1266a3c836f161a0908bfb4a6c
Caua-Giovani <caua.g.andrade7@aluno.senai.br>
2026-03-27 15:53:55 -0300

    Update database connection to support cloud options
    Refactored database connection logic to support cloud and local environments.

--- database/conexao.py
+++ database/conexao.py
@@ -1,14 +1,23 @@
 import mysql.connector
 
 def conectar():
-    
-    conexao=mysql.connector.connect(
-            host="localhost",
-            port=3306,
-            user="root",
-            password="root",
-            database="Musicablo"
-        )
+    tipo_conexao = "NUVEM"
+    if tipo_conexao == "LOCAL":
+        conexao=mysql.connector.connect(
+                host="localhost",
+                port=3306,
+                user="root",
+                password="root",
+                database="Musicablo"
+            )
+    else:
+        conexao=mysql.connector.connect(
+                host="mysql-289071e4-servidor-caua.j.aivencloud.com",
+                port=16660,
+                user="avnadmin",
+                password="AVNS_Q-U2mYvJ-2Fv-uGD6DB",
+                database="Musicablo"
+            )
     
     cursor=conexao.cursor(dictionary=True)
 
